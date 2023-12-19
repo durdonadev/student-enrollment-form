1756790373a1639214f968d40f908a34156a68aa
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-19 01:16:48 -0500

    Add Student

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { prisma } from "./src/prisma/index.js";
 
 dotenv.config();
 
@@ -9,6 +10,35 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
+
+app.post("/students", async (req, res) => {
+    const {
+        body: { firstName, lastName, email, className }
+    } = req;
+    if (!firstName || !lastName || !email || !className) {
+        res.status(400).json({
+            message: "All fields are required!"
+        });
+        return;
+    }
+
+    try {
+        const student = await prisma.student.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                className
+            }
+        });
+
+        res.status(201).json({
+            data: student
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
