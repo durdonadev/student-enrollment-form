bda2e9dd684e027cee56f1fb83d6364ac8dc78dd
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-19 01:31:13 -0500

    Delete A Student

--- index.js
+++ index.js
@@ -127,6 +127,38 @@ app.patch("/students/:id", async (req, res) => {
     }
 });
 
+app.delete("/students/:id", async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+
+    try {
+        const student = await prisma.student.findUnique({
+            where: {
+                id: id
+            }
+        });
+        if (!student) {
+            res.status(404).json({
+                message: "Student is not found"
+            });
+            return;
+        }
+
+        await prisma.student.delete({
+            where: {
+                id: id
+            }
+        });
+
+        res.status(203).send();
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
 });
